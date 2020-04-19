aafea77c2ce7a7970d47ce7dc2d9477be107010d
Wagner Alcantara <wagner.analise@gmail.com>
2020-04-19 18:57:35 -0300

    Melhorando a impressão na tela

--- xadrez-console/xadrez-console/Program.cs
+++ xadrez-console/xadrez-console/Program.cs
@@ -4,9 +4,18 @@ using xadrez;
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            PosicaoXadre pos = new PosicaoXadre('c',7);
-            Console.WriteLine(pos);
-                Console.WriteLine(pos.toPosicao())  ;
+            try {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 3));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                Tela.imprimirTabuleiro(tab);
+            } catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
 
 
             Console.ReadLine();
